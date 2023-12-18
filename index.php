c5226ebbeb327354b44f0e96ca52c00519675a61
GiorgioMos <giorgiomosca2@gmail.com>
2023-12-18 12:09:31 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+
+    <form action="censor.php" method="GET">
+        <label for="paragraph">Inserisci un paragrafo</label>
+        <input type="text" id="paragraph" name="paragraph">
+
+        <label for="badword">Inserisci la parola da censurare</label>
+        <input type="text" id="badword" name="badword">
+
+        <button type="submit">Invia</button>
+    </form>
+
+</body>
+
+</html>
